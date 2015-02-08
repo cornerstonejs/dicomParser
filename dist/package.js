e10fbf7c2bccf762adb088f998f8642296185025
chafey <chafey@gmail.com>
2015-02-08 07:09:45 -0600

    bumped meteor version

--- dist/package.js
+++ dist/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'chafey:dicom-parser',
   summary: 'Javascript parser for DICOM Part 10 data',
-  version: '0.4.3',
+  version: '0.5.1',
   git: 'https://github.com/chafey/dicomParser.git/'
 });
 
--- meteor/package.js
+++ meteor/package.js
@@ -2,11 +2,12 @@ Package.describe({
   name: 'chafey:dicom-parser',
   summary: 'Javascript parser for DICOM Part 10 data',
   version: '0.5.1',
-  git: 'https://github.com/chafey/dicomParser.git/'
+  git: 'https://github.com/chafey/dicomParser.git/',
+  documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
-  api.addFiles('dicomParser.js');
+  api.addFiles('../dist/dicomParser.js');
 });
 
