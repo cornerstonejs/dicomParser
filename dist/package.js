0021a893e1fe1230610b511f062e99cbc490e7d4
chafey <chafey@gmail.com>
2015-04-06 06:18:59 -0500

    Version 1.0.0

--- dist/package.js
+++ dist/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'chafey:dicom-parser',
   summary: 'Javascript parser for DICOM Part 10 data',
-  version: '0.8.3',
+  version: '1.0.0',
   git: 'https://github.com/chafey/dicomParser.git/',
   documentation: null
 });
