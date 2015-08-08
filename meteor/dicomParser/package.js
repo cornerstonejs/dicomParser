7b9782c8f8ed14bfa2813d2c869a1f0e87c9d9c8
chafey <chafey@gmail.com>
2015-08-08 14:40:12 -0500

    getting meteor working

---
+++ meteor/dicomParser/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'chafey:dicom-parser',
+  summary: 'Javascript parser for DICOM Part 10 data',
+  version: '1.1.3',
+  git: 'https://github.com/chafey/dicomParser.git/',
+  documentation: null
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.addFiles('../../dist/dicomParser.js');
+  api.export('dicomParser');
+});
+
