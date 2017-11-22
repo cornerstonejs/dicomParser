a1e77ab1942e549e13e49346cfda5ed2292b0a08
Erik Ziegler <erik.sweed@gmail.com>
2017-11-22 12:29:22 +0100

    Add eslint-plugin-import to force paths to resolve

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'mocha': true
   },
   'extends': 'eslint:recommended',
+  'plugins': ['import'],
   'parserOptions': {
     'sourceType': 'module'
   },
@@ -69,7 +70,16 @@ module.exports = {
     'id-length': 'off',
     'id-match': 'warn',
     'indent': ['warn', 2],
-    'init-declarations': 'warn',
+    //'init-declarations': 'warn',
+    'import/default': 'warn',
+    'import/export': 'warn',
+    'import/extensions': ['warn', { "js": "always" }],
+    'import/first': 'warn',
+    'import/named': 'warn',
+    'import/namespace': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-unresolved': 'warn',
+    'import/no-webpack-loader-syntax': 'warn',
     'jsx-quotes': 'warn',
     'key-spacing': 'warn',
     'keyword-spacing': [
