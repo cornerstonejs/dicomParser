472505ca2c35c0a9d1c9ae5a60f3d06da1503fb1
Gustavo André Lelis <galelis@gmail.com>
2018-04-12 03:28:57 -0300

    Switch from PhantomJS to ChromeHeadless for tests (#84)

--- config/karma/karma-base.js
+++ config/karma/karma-base.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpackConfig = require('../webpack');
 
+/* eslint no-process-env:0 */
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 // Deleting output.library to avoid "Uncaught SyntaxError: Unexpected token /" error
 // when running testes (var test/foo_test.js = ...)
 delete webpackConfig.output.library;
@@ -26,7 +29,6 @@ module.exports = {
   plugins: [
     'karma-webpack',
     'karma-mocha',
-    'karma-phantomjs-launcher',
     'karma-chrome-launcher',
     'karma-firefox-launcher',
     'karma-coverage'
