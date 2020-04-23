d1ec839000631c0e109ed8c7e7e4c58b57202d69
Sanders DeNardi <sdenardi@summusglobal.com>
2020-04-23 11:11:28 -0400

    make index function parameter optional

--- index.d.ts
+++ index.d.ts
@@ -23,32 +23,32 @@ declare module 'dicom-parser' {
     /**
      * Finds the element for tag and returns an unsigned int 16 if it exists and has data. Use this function for VR type US.
      */
-    uint16: (tag: string, index: number) => number;
+    uint16: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and returns an signed int 16 if it exists and has data. Use this function for VR type SS.
      */
-    int16: (tag: string, index: number) => number;
+    int16: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and returns an unsigned int 32 if it exists and has data. Use this function for VR type UL.
      */
-    uint32: (tag: string, index: number) => number;
+    uint32: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and returns an signed int 32 if it exists and has data. Use this function for VR type SL.
      */
-    int32: (tag: string, index: number) => number;
+    int32: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and returns a 32 bit floating point number if it exists and has data. Use this function for VR type FL.
      */
-    float: (tag: string, index: number) => number;
+    float: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and returns a 64 bit floating point number if it exists and has data. Use this function for VR type FD.
      */
-    double: (tag: string, index: number) => number;
+    double: (tag: string, index?: number) => number;
 
     /**
      * Returns the Value Multiplicity of an elements - the number of values in a multi-valued element.
@@ -58,22 +58,22 @@ declare module 'dicom-parser' {
     /**
      * Finds the element for tag and returns a string if it exists and has data. Use this function for VR types AE, CS, SH, and LO.
      */
-    string: (tag: string, index: number) => string;
+    string: (tag: string, index?: number) => string;
 
     /**
      * Finds the element for tag and returns a string with the leading spaces preserved and trailing spaces removed if it exists and has data. Use this function for VR types UT, ST, and LT.
      */
-    text: (tag: string, index: number) => string;
+    text: (tag: string, index?: number) => string;
 
     /**
      * Finds the element for tag and parses a string to a float if it exists and has data. Use this function for VR type DS.
      */
-    floatString: (tag: string, index: number) => number;
+    floatString: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and parses a string to an integer if it exists and has data. Use this function for VR type IS.
      */
-    intString: (tag: string, index: number) => number;
+    intString: (tag: string, index?: number) => number;
 
     /**
      * Finds the element for tag and parses an element tag according to the 'AT' VR definition if it exists and has data. Use this function for VR type AT.
