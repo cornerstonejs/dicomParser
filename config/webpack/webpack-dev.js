d9825704e1e49670ed598f7ca6d3028765791bce
Bryan Cool <yagni@users.noreply.github.com>
2021-10-06 06:57:32 -0500

    fix: Upgrade dependencies to most recent supported. (#184)

--- config/webpack/webpack-dev.js
+++ config/webpack/webpack-dev.js
@@ -5,7 +5,9 @@ const baseConfig = require('./webpack-base');
 const devConfig = {
   devServer: {
     hot: true,
-    publicPath: '/dist/'
+    static: {
+      publicPath: '/dist/'
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin({})
